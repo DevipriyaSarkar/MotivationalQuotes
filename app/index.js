35563545ee5a32c3d637dd17b13161c701aceb7f
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2016-03-20 17:48:13 +0530

    add motivation package

--- app/index.js
+++ app/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 
+var motivations = require('motivations');
+
 app.get('/', function(request, response){
   response.send("Meow!");
 });
 
 app.get('/hello', function(request, response){
   response.send("Hello World!");
+});
+
+app.get('/mot', function(request, response){
+  response.send(motivations[3]);
 });
 
 module.exports = app;
