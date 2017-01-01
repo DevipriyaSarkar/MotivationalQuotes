b8b2b8209eb9da5d09d3a240f86371e961d0f274
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-01-02 03:11:59 +0530

    fix: fix App Boot Timeout error on Heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var port = '8080';
-var host = '0.0.0.0';
+const port = process.env.PORT || '8080';
+const host = process.env.HOST || '0.0.0.0';
 
 var express = require('express');
 var path = require('path');
