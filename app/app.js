55e79aedfa8c2dd5be4f1909fac513118daa202a
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-01-02 02:29:53 +0530

    feat: add template rendering using hogan

--- app/app.js
+++ app/app.js
@@ -4,8 +4,14 @@ var app = express();
 var motivations = require('motivations');
 var pickOne = require('pick-one');
 
+var bgImages = ["image1", "image2", "image3", "image4", "image5"];
+
 app.get('/', function(request, response){
-  response.send(pickOne(motivations));
+  response.render('index', {
+	  image: pickOne(bgImages),
+	  quote: pickOne(motivations)
+  });
+  
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var port = '8080';
 var host = '0.0.0.0';
 
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
 var app = express();
 
-app.use(require('./app/index.js'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.enable('view cache');
+app.engine('html', require('hogan-express'))
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./app/app.js'));
 
 app.listen(port, host);
 console.log("Server is listening...");
