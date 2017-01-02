7390a1dda35b987084f904a79bf43e5493c96413
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-01-02 20:37:52 +0530

    chore: change the motivation generator package

--- app/app.js
+++ app/app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
 
-var motivations = require('motivations');
+var motivation = require('motivation');
 var pickOne = require('pick-one');
 
 var bgImages = ["image1", "image2", "image3", "image4", "image5"];
 
+var quoteList = motivation.getAll();
+
 app.get('/', function(request, response){
   response.render('index', {
 	  image: pickOne(bgImages),
-	  quote: pickOne(motivations)
+	  quote: pickOne(quoteList).text,
+	  author: pickOne(quoteList).author
   });
   
 });
